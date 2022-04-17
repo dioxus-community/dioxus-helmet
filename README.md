316c2881cc9dc1a520011108a03125b6adb5ef5e
Saicu <sam@saicu.de>
2022-04-17 04:35:37 +0200

    version 0.1.0

---
+++ README.md
@@ -0,0 +1,59 @@
+# dioxus-helmet
+
+Inspired by react-helmet, this small component allows you to place elements in the **head** of your code.
+
+## Configuration
+
+Add the package as a dependency in your `Cargo.toml`.
+
+### Web:
+```
+dioxus-helmet = { git = "https://github.com/saicu/dioxus-helmet" }
+```
+
+### ~~Desktop:~~ (doesnt work yet)
+```
+dioxus-helmet = { git = "https://github.com/saicu/dioxus-helmet", default-features = false, features = ["desktop"] }
+```
+
+## Usage
+
+Import it in your code 
+```
+use dioxus_helmet::Helmet;
+```
+
+Then you can just use in in your components like this
+
+``` 
+    cx.render(rsx! {
+        div {
+            Helmet {
+                link { rel: "stylesheet", href: "/style.css" }
+                title { "Helmet" }
+                style {
+                    [r#"
+                        body {
+                            color: blue;
+                        }
+                        a {
+                            color: red;
+                        }
+                    "#]
+                }
+            },
+            p { "Hello, world!" }
+        }
+    })
+
+```
+
+Any children passed to the helmet component will be placed in the `<head></head>` of your document.
+
+## License
+
+This project is dual licensed under the MIT license.
+
+## Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in dioxus-helmet by you, shall be licensed as MIT, without any additional terms or conditions
---
+++ src/lib.rs
@@ -0,0 +1,65 @@
+use dioxus::prelude::*;
+
+#[derive(Props)]
+pub struct HelmetProps<'a> {
+    children: Element<'a>,
+}
+
+/// Any children passed to the helmet component will be moved into the **head** area of your document.
+#[allow(non_snake_case)]
+pub fn Helmet<'a>(cx: Scope<'a, HelmetProps<'a>>) -> Element {
+    #[cfg(feature = "web")]
+    let eval = dioxus::web::use_eval(&cx);
+
+    #[cfg(feature = "desktop")]
+    let eval = dioxus::desktop::use_eval(&cx);
+
+    if let Some(VNode::Fragment(fragment)) = &cx.props.children {
+        fragment.children.iter().for_each(|child| {
+            if let VNode::Element(element) = child {
+                let tag = element.tag;
+                let attributes: String = element
+                    .attributes
+                    .iter()
+                    .map(|attribute| {
+                        let name = attribute.name;
+                        let value = attribute.value;
+                        format!("el.setAttribute('{name}', '{value}');")
+                    })
+                    .collect();
+                let children = &*element.children;
+                                    
+                let inner_text = if children.len() == 1 {
+                    match children[0] {
+                        VNode::Text(text) => {
+                            let text = text.text;
+                            format!("el.innerText = '{text}'")
+                        },
+                        VNode::Fragment(fragment) if fragment.children.len() == 1 => {
+                            if let VNode::Text(text) = fragment.children[0] {
+                                let text = text.text.replace("}\n", "} ").replace('\n', "");
+                                format!("el.innerHTML = '{text}'")
+                            } else {
+                                "".to_owned()
+                            }
+                        }
+                        _ => { "".to_owned() }
+                    }
+                } else {
+                    "".to_owned()
+                };                   
+
+                eval(format!(
+                    r#"
+                        let el = document.createElement('{tag}')
+                        {attributes}
+                        {inner_text}
+                        document.head.appendChild(el)
+                    "#
+                ));
+            }
+        });
+    }
+
+    None
+}
