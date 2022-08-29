419e28cd3a65c1e1f0eb6fc050540b063d17ac01
Sam Schmeink <sam@saicu.de>
2022-08-29 15:24:03 +0200

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -51,45 +51,58 @@ pub fn Helmet<'a>(cx: Scope<'a, HelmetProps<'a>>) -> Element {
         if let Some(window) = web_sys::window() {
             if let Some(document) = window.document() {
                 if let Some(head) = document.head() {
-                    if let Some(VNode::Fragment(fragment)) = &cx.props.children {
-                        fragment.children.iter().for_each(|child| {
-                            if let VNode::Element(element) = child {
-                                if let Ok(new_element) = document.create_element(element.tag) {
-                                    element.attributes.iter().for_each(|attribute| {
-                                        let name = attribute.name;
-                                        let value = attribute.value;
-                                        let _ = new_element.set_attribute(name, value);
-                                    });
-
-                                    match element.children.first() {
-                                        Some(VNode::Text(text)) => {
-                                            new_element.set_text_content(Some(text.text));
-                                        }
-                                        Some(VNode::Fragment(fragment))
-                                            if fragment.children.len() == 1 =>
-                                        {
-                                            if let Some(VNode::Text(text)) =
-                                                fragment.children.first()
-                                            {
-                                                let inner = text
-                                                    .text
-                                                    .replace("}\n", "} ")
-                                                    .replace('\n', "");
-
-                                                new_element.set_inner_html(&inner);
-                                            };
-                                        }
-                                        _ => {}
-                                    };
-
-                                    let _ = head.append_child(&new_element);
-                                }
-                            }
+                    if let Some(new_children) = extract_elements(&document, &cx.props.children) {
+                        new_children.iter().for_each(|child| {
+                            let _ = head.append_child(child);
                         });
                     }
                 }
             }
         }
+    }
+
+    None
+}
+
+fn extract_elements<'a>(
+    document: &web_sys::Document,
+    children: &Element<'a>,
+) -> Option<Vec<web_sys::Element>> {
+    if let Some(VNode::Fragment(fragment)) = &children {
+        let elements = fragment
+            .children
+            .iter()
+            .flat_map(|child| {
+                if let VNode::Element(element) = child {
+                    if let Ok(new_element) = document.create_element(element.tag) {
+                        element.attributes.iter().for_each(|attribute| {
+                            let name = attribute.name;
+                            let value = attribute.value;
+                            let _ = new_element.set_attribute(name, value);
+                        });
+
+                        match element.children.first() {
+                            Some(VNode::Text(text)) => {
+                                new_element.set_text_content(Some(text.text));
+                            }
+                            Some(VNode::Fragment(fragment)) if fragment.children.len() == 1 => {
+                                if let Some(VNode::Text(text)) = fragment.children.first() {
+                                    let inner = text.text.replace("}\n", "} ").replace('\n', "");
+
+                                    new_element.set_inner_html(&inner);
+                                };
+                            }
+                            _ => {}
+                        };
+
+                        return Some(new_element);
+                    }
+                }
+                None
+            })
+            .collect();
+
+        return Some(elements);
     }
 
     None
